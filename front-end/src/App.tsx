4bbc1d2022d30b88da59eefd1f0c1f23f09c4737
Guilherme Martins <guimsl2004@gmail.com>
2024-07-09 17:45:48 -0300

    add email por formulario em um modal

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,11 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2 } from 'lucide-react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus } from 'lucide-react'
 import './App.css'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
 export function App() {
   const [isGestInputOpen, setIsGestInputOpen] =  useState(false)
-
+  const [isGestModal, setIsGestModal] =  useState(false)
+  const [emalsToInvite, setEmailsToInvate] = useState(['email@email.com'])
 
   function openGestInput(){
     setIsGestInputOpen(true)
@@ -12,6 +13,29 @@ export function App() {
   function closeGestInput(){
     
     setIsGestInputOpen(false)
+  }
+  function openGestModal(){
+    setIsGestModal(true)
+  }
+  function closeGestModal(){
+    setIsGestModal(false)
+  }
+  function addEmail(event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    const email = data.get('email')?.toString()
+    if (!email){
+      return
+    }
+
+    if (emalsToInvite.includes(email)){
+      return
+    }
+    setEmailsToInvate([
+      ...emalsToInvite,
+      email
+    ])
+    event.currentTarget.reset()
   }
   return (
     <div className='w-full h-screen flex items-center justify-center shadow-shape bg-patter bg-no-repeat bg-center'>
@@ -50,10 +74,10 @@ export function App() {
 
           {isGestInputOpen ? (
             <div className='h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3'>
-              <div className='flex items-center gap-2 flex-1'>
+              <button type='button' onClick={openGestModal} className='flex items-center gap-2 flex-1 text-left'>
                 <UserRoundPlus className='size-5 text-zinc-400'/>
-                <input type='text' placeholder='Quem estará na viagem?'className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1'/>
-              </div>
+                <span className='text-zinc-400 text-lg flex-1'>Quem estará na viagem?</span>
+              </button>
 
           
 
@@ -72,7 +96,50 @@ export function App() {
         com nossos <a className='text-zinc-300 underline' href='#'>termos de uso</a> e <a className='text-zinc-300 underline' href='#'>políticas de privacidade.</a>
         </p>
       </div>
+
+      {isGestModal ? ( 
+        <div className='fixed inset-0 bg-black/50 flex items-center justify-center'>
+          <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+            <div className='space-y-2'>
+              <div className='flex items-center justify-between'>
+                <h2 className='text-lg font-semibold'>Selecionar convidados</h2>
+                <button onClick={closeGestModal}>
+                  <X className='size-5 text-zinc-400'></X>
+
+                </button>
+              </div>
+              <p className='text-sm text-zinc-400'>Os convidados irão receber e-mails para confirmar a participação na viagem.</p>
+            </div>
+            <div className='flex flex-wrap gap-2'>
+              {emalsToInvite.map(email => {
+                return (
+                  <div key={email} className='py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2'>
+                    <span className='text-zinc-300'>{email}</span>
+                    <button type='button'>
+                      <X className='size-5 text-zinc-400'></X>
+                    </button>
+                  </div>
+                )
+              })}
+            </div>
+            <div className='w-full h-px bg-zinc-800'/>
+
+            <form onSubmit={addEmail} className='p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+              <div className='px-2 flex items-center flex-1 gap-2'>
+                <AtSign className='text-zinc-400 size-5'/>
+                <input type='email' name='email' placeholder='Digite o email do convidado'className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1'/>
+              </div>
+              
+              <button type='submit' className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+                Convidar
+                <Plus className='size-5'/>
+              </button>
+            </form>
+          </div>
+        </div>
+      ) : null}
     </div>
+
     
   )
 }
